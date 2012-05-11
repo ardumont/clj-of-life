33c41e24f090ffd9d42244f587a6692fd83ed44a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-05-11 16:24:54 +0200

    Some idea from den.

--- src/clj_of_life/core.clj
+++ src/clj_of_life/core.clj
@@ -110,6 +110,7 @@
 
 ;; ------------------------ Side effects -------------------------
 
+;; improvment proposition from denlab:
 ;; pour les couleurs je te conseilles de tout simplement remplacer les "1" par des triplets RGB : [1 2 3]
 ;; comme ca pas trop dur a changer au niveau de l'implem
 
@@ -117,7 +118,6 @@
 ;; par example, la moynne de rouge [255 0 0] et bleu [0 0 255]
 ;; c'est [255/2 0/2 255/2] = [124 0 124] (violet je crois)
 ;; ensuite lors de l'affichage pour tu a constructeur de couleur qui prend le rgb, donc dans ce cas : (java.awt.Color. 124 0 124)
-
 
 (def *size-cell 10)
 
