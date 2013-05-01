ee0953a9ac7e8752b8e73b9b69b587db451a95c5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-01 16:26:01 +0200

    Upgrade to the latest clojure and midje version.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-of-life/clj-of-life "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.0"]]
   :description "A simple game of life implementation")
